506c4fd0b91cefc3702f52e40a6a99319cba98ca
damons <damon@sicore.com>
2024-06-07 05:07:46 +0000

    Fixed bug in arg count.

--- scripts/bond_validator.sh
+++ scripts/bond_validator.sh
@@ -4,7 +4,7 @@
 source ./environment.sh
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 5 ]; then
+if [ "$#" -ne 6 ]; then
     echo "Usage: $0 <PRIVATE_KEY> <VALIDATOR_ADDRESS> <WITHDRAW_ADDRESS> <AMOUNT> <COMMISSION> <NAME>"
     exit 1
 fi
